e8b4526303caa572cc248df32d98546f01724a73
DiegoFLM <diego.ledesma@correounivalle.edu.co>
2024-04-27 19:25:26 -0500

    Test for env variable updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 // const uri = process.env.MONGODB_URI;
-uri = "mongodb+srv://diegoledesma:IxmsJjppmR7O8fAO@cluster0.nulgm4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const uri = "mongodb+srv://diegoledesma:IxmsJjppmR7O8fAO@cluster0.nulgm4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 
